816b1768e4732c6b3e055ffa90931cbfa3de51f2
wjmanzon <wilsonjr.manzon@gmail.com>
2024-03-25 02:43:01 -0600

    3.2.2 Creating an Altair plot

--- chapter1.py
+++ chapter1.py
@@ -1,14 +1,14 @@
-# loading in libraries
+# load libraries
 import pandas as pd
 import altair as alt
 
 # personal note: the above import statements cannot be run if the modules haven't been installed. You can do that by running this to the command line: python -m pip install pandas altair
 
-# loading in csv file
+# load csv file
 url = "https://github.com/byuidatascience/data4python4ds/raw/master/data-raw/mpg/mpg.csv"
 mpg = pd.read_csv(url)
 
-# creating the chart
+# create the chart
 chart = (alt.Chart(mpg)
   .encode(
     x='displ', 
@@ -16,5 +16,8 @@ chart = (alt.Chart(mpg)
   .mark_circle()
 )
 
-# saving the chart
+# call the chart so it shows on the interactive window
+chart
+
+# save the chart in screenshots folder. That folder should have been precreated.
 chart.save("screenshots/altair_viz_1.png")
